518ab79774ca061b04e36494524b693b1c60f68c
RAVI VARDHAN VANGARU <64185173+Ravivardhan@users.noreply.github.com>
2021-07-18 17:20:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from chatbot import chatbot
+!pip install ChatterBot
+from chatterbot import ChatBot
+!pip install ChatterBot-corpus
 
+
+chatbot = ChatBot(
+    'CoronaBot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch',
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand. I am still learning.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+
+# Training With Own Questions 
+from chatterbot.trainers import ListTrainer
+
+trainer = ListTrainer(chatbot)
+training_data_quesans = open('training_data/ques_ans.txt').read().splitlines()
+training_data_personal = open('training_data/personal_ques.txt').read().splitlines()
+
+training_data = training_data_quesans + training_data_personal
+
+trainer = ListTrainer(chatbot)
+trainer.train(training_data)
+
+
+
+
+
+!pip install flask_ngrok
+
+from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.static_folder = 'static'
-
+run_with_ngrok(app)
 @app.route("/")
 def home():
     return render_template("index.html")
